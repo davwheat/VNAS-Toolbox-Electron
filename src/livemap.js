f7200101e3906577960f42bd76dc3c8e54234b49
David Wheatley <blokmaster.help@gmail.com>
2019-08-13 16:23:07 +0100

    Add more live map imagery layers

--- src/livemap.js
+++ src/livemap.js
@@ -1,6 +1,50 @@
 const L = require("leaflet/dist/leaflet");
 require("leaflet-rotatedmarker");
 
+var BingLayer = L.TileLayer.extend({
+  getTileUrl: function(tilePoint) {
+    //this._adjustTilePoint(tilePoint);
+    return L.Util.template(this._url, {
+      s: this._getSubdomain(tilePoint),
+      q: this._quadKey(tilePoint.x, tilePoint.y, this._getZoomForUrl())
+    });
+  },
+  _quadKey: function(x, y, z) {
+    var quadKey = [];
+    for (var i = z; i > 0; i--) {
+      var digit = "0";
+      var mask = 1 << (i - 1);
+      if ((x & mask) != 0) {
+        digit++;
+      }
+      if ((y & mask) != 0) {
+        digit++;
+        digit++;
+      }
+      quadKey.push(digit);
+    }
+    return quadKey.join("");
+  }
+});
+
+function GenerateFlightPopup(
+  flightNum,
+  from,
+  to,
+  alt,
+  gs,
+  hdg,
+  userId,
+  name = undefined,
+  percent = undefined
+) {
+  return `<b>${flightNum} | ${from} → ${to}${
+    name == undefined ? "" : ` (${percent})`
+  }</b><br/>${alt}ft ASL | ${gs}kt | HDG ${hdg}°<br/>${userId}${
+    name == undefined ? "" : ` - ${name}`
+  }`;
+}
+
 let markers = [];
 
 $("#liveMapToggleVnas").value(GetSetting("show_all_vnas_flights_on_map"));
@@ -13,24 +57,69 @@ $("#liveMapToggleVnas").click(() => {
   $("#Settings input[data-setting=show_all_vnas_flights_on_map]").click();
 });
 
-let osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-let osmAttribution = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+const osm = new L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      "Map data & imagery: <a href=\"#\" onclick=\"require('electron').shell.openExternal('https://openstreetmap.org')\">© OpenStreetMap contributors</a>",
+    minZoom: 1,
+    maxZoom: 19
+  }
+);
 
-let osm = new L.TileLayer(osmUrl, {
-  attribution: osmAttribution,
-  minZoom: 0,
-  maxZoom: 19
-});
+const openTopo = new L.tileLayer(
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      "Map Data: <a href=\"#\" onclick=\"require('electron').shell.openExternal('https://openstreetmap.org/copyright')\">© OpenStreetMap contributors</a> | Imagery: <a href=\"#\" onclick=\"require('electron').shell.openExternal('http://opentopomap.org/')\">© OpenTopoMap</a> (<a href=\"#\" onclick=\"require('electron').shell.openExternal('https://creativecommons.org/licenses/by-sa/3.0/')\">CC-BY-SA</a>)",
+    minZoom: 1,
+    maxZoom: 17
+  }
+);
+
+const satellite = new L.tileLayer(
+  "https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}{r}.jpg?key=FCxcrAwhY1uAsPwUr6r3",
+  {
+    attribution:
+      'Map Data: <a href="#" onclick="require(\'electron\').shell.openExternal(\'https://www.openstreetmap.org/copyright\')">© OpenStreetMap contributors</a> | Imagery: <a href="#" onclick="require(\'electron\').shell.openExternal(\'https://www.maptiler.com/copyright/\')" target="_blank">© MapTiler</a>',
+    tileSize: 512,
+    minZoom: 1,
+    maxZoom: 19,
+    zoomOffset: -1
+  }
+);
+
+const bingSatellite = new BingLayer(
+  "http://r{s}.ortho.tiles.virtualearth.net/tiles/a{q}.jpeg?g=136",
+  {
+    minZoom: 1,
+    maxZoom: 19,
+    subdomains: [0, 1, 2, 3],
+    attribution:
+      "<a href=\"#\" onclick=\"require('electron').shell.openExternal('http://bing.com/maps')\">© Bing Maps</a>"
+  }
+);
+
+const mapLayers = {
+  OpenStreetMap: osm,
+  OpenTopography: openTopo,
+  Satellite: satellite,
+  "HD Satellite (no labels)": bingSatellite
+};
 
 // Start at Gatwick Lat/Lon
-const mymap = L.map("leafletMap", {
-  attributionControl: false,
-  minZoom: 0,
-  maxZoom: 19,
+const leafletLiveMap = L.map("leafletMap", {
   center: [51.148056, -0.190278],
-  zoom: 11,
-  layers: [osm]
+  zoom: 8,
+  layers: [osm],
+  maxBoundsViscosity: 0.5,
+  zoomAnimationThreshold: 19,
 });
+leafletLiveMap.setMaxBounds([[-90, -180], [90, 180]]);
+
+leafletLiveMap.attributionControl.setPrefix(false);
+
+L.control.layers(mapLayers).addTo(leafletLiveMap);
 
 const icon = L.Icon.extend({
   options: {
@@ -60,7 +149,7 @@ const b738 = new icon({ iconUrl: "img/planes/737-icon.svg" }),
       if (!GetSetting("show_all_vnas_flights_on_map")) {
         markers.forEach(marker => {
           if (marker == undefined) return;
-          mymap.removeLayer(marker);
+          leafletLiveMap.removeLayer(marker);
         });
         markers = [];
       }
@@ -69,15 +158,19 @@ const b738 = new icon({ iconUrl: "img/planes/737-icon.svg" }),
         icon: myFlight.AircraftType.startsWith("B78") ? b788 : b738,
         rotationAngle: myFlight.Heading
       }).bindPopup(
-        `You! // ${myFlight.ArrivalIcao} → ${
-          myFlight.DepartureIcao
-        }<br/>${Math.floor(myFlight.AltitudeASL)}ft | ${Math.floor(
-          myFlight.SpeedGS
-        )}kts | ${Math.round(myFlight.Heading)}°`
+        GenerateFlightPopup(
+          "You!",
+          myFlight.DepartureIcao,
+          myFlight.ArrivalIcao,
+          Math.floor(myFlight.AltitudeASL),
+          Math.floor(myFlight.SpeedGS),
+          Math.round(myFlight.Heading),
+          GetSetting("vnas_username")
+        )
       );
 
       if (!GetSetting("show_all_vnas_flights_on_map")) {
-        temp.addTo(mymap);
+        temp.addTo(leafletLiveMap);
         return;
       }
     }
@@ -87,7 +180,7 @@ const b738 = new icon({ iconUrl: "img/planes/737-icon.svg" }),
 
       markers.forEach(marker => {
         if (marker == undefined) return;
-        mymap.removeLayer(marker);
+        leafletLiveMap.removeLayer(marker);
       });
       markers = [temp];
 
@@ -100,18 +193,24 @@ const b738 = new icon({ iconUrl: "img/planes/737-icon.svg" }),
             icon: flight.aircraft.aircraft.startsWith("B78") ? b788 : b738,
             rotationAngle: flight.aircraft.heading
           }).bindPopup(
-            `${flight.flightNum} // ${flight.dep.icao} → ${
-              flight.arr.icao
-            }<br/>${flight.aircraft.altitude}ft | ${
-              flight.aircraft.gndSpeed
-            }kts | ${flight.aircraft.heading}°`
+            GenerateFlightPopup(
+              flight.flightNum,
+              flight.dep.icao,
+              flight.arr.icao,
+              flight.aircraft.altitude,
+              flight.aircraft.gndSpeed,
+              flight.aircraft.heading,
+              "NAX" + flight.userId,
+              flight.userName,
+              flight.progress
+            )
           )
         );
       });
 
       markers.forEach(marker => {
         if (marker == undefined) return;
-        marker.addTo(mymap);
+        marker.addTo(leafletLiveMap);
       });
     });
   }
